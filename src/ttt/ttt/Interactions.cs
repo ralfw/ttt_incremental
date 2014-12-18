43c6522c8a55359ac5e80eea6e858e841b5b579d
Ralf Westphal <info@ralfw.de>
2014-12-18 14:43:37 +0100

    Implemented check

--- src/ttt/ttt/Interactions.cs
+++ src/ttt/ttt/Interactions.cs
@@ -19,7 +19,7 @@ namespace ttt
         {
             Reset_fields();
             var player = Identify_current_player();
-            return Generate_gamestate(player);
+            return Generate_gamestate_for_player(player);
         }
 
         private void Reset_fields()
@@ -30,10 +30,26 @@ namespace ttt
 
         public Gamestate Draw(int coordinate)
         {
-            var player = Identify_current_player();
-            Place_symbol_on_field(player, coordinate);
-            player = Identify_current_player();
-            return Generate_gamestate(player);
+            Gamestate gs = null;
+            Check_for_occupied_field(coordinate,
+                () => {
+                    var player = Identify_current_player();
+                    Place_symbol_on_field(player, coordinate);
+                    player = Identify_current_player();
+                    gs = Generate_gamestate_for_player(player);
+                },
+                msg => { gs = Generate_gamestate_for_status(msg); });
+            return gs;
+        }
+
+        private void Check_for_occupied_field(int coordinate,
+                                              Action onUnoccupied,
+                                              Action<string> onOccupied)
+        {
+            if (_fieldSelections.Any(fs => fs.Coordinate == coordinate))
+                onOccupied("Invalid selection!");
+            else
+                onUnoccupied();
         }
 
         private Players Identify_current_player()
@@ -49,14 +65,20 @@ namespace ttt
         }
 
 
-        Gamestate Generate_gamestate(Players currentPlayer)
+        Gamestate Generate_gamestate_for_player(Players currentPlayer)
+        {
+            var msg = string.Format("Player: {0}", currentPlayer.ToString());
+            return Generate_gamestate_for_status(msg);
+        }
+
+        Gamestate Generate_gamestate_for_status(string statusMsg)
         {
             var gs = new Gamestate();
             foreach (var t in _fieldSelections)
                 gs.Board.Fieldvalues[t.Coordinate] = t.Player == Players.X
                                                          ? Fieldvalues.X
                                                          : Fieldvalues.O;
-            gs.Message = string.Format("Player: {0}", currentPlayer.ToString());
+            gs.Message = statusMsg;
             return gs;
         }
     }
